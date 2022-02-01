88c64823ab5abc7421701892fdb4947a522956ca
JaegerDaFBoi <jcamilo2696@gmail.com>
2022-01-31 19:42:13 -0500

    construyendo variables

---
+++ operadores_aritmeticos.php
@@ -0,0 +1,29 @@
+<?php 
+$num1 = 5;
+$num2 = 4;
+$suma=$num1+$num2;
+$resta=$num1-$num2;
+$multiplicaion=$num1*$num2;
+$division=$num1/$num2;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>el resultado es: </h1>
+<hr>
+<p>La suma es:  <?php echo $suma ?></p>
+<p>La resta es: <?php echo $resta ?></p>
+<p>La la division es:<?php echo $division ?> </p>
+<p>La multiplicacion es:<?php echo $multiplicaion ?> </p>
+    <h5>estas respuestas son de las operaciones de php</h5>
+</body>
+</html>
+
