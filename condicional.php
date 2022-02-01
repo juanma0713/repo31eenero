a03b08b5886f144f16c4dad89db018b2d8953f0c
JaegerDaFBoi <jcamilo2696@gmail.com>
2022-01-31 20:21:06 -0500

    variables

---
+++ condicional.php
@@ -0,0 +1,15 @@
+<?php
+
+$edad=rand(1,103);
+
+if ($edad>=18){
+    echo "su edad es $edad";
+    echo "<br>";
+    echo "usted es mayor de edad";
+}else {
+
+    echo " su edad es $edad";
+    echo "<br>";
+    echo "usted es menor de edad";
+}
+
